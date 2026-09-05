bc87d9302d0e746c4c1b93aaedd8fa9e53e4c6ac
zeckofc143-hub <zeck.ofc143@gmail.com>
2026-09-05 03:22:42 -0300

    style: load real creature cover styles

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import AdvancedPlanner from './AdvancedPlanner';
 import CommunityResearchHub from './CommunityResearchHub';
 import CreatureCatalog from './CreatureCatalog';
 import './index.css';
+import './creatureCatalogV2.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
