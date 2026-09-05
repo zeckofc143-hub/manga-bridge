3183eda0bda8e7d840c8452cb28e3d3824b98c3b
zeckofc143-hub <zeck.ofc143@gmail.com>
2026-09-05 02:33:49 -0300

    feat: mount Pocket Ants community research hub

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import Enhancements from './Enhancements';
 import AdvancedPlanner from './AdvancedPlanner';
+import CommunityResearchHub from './CommunityResearchHub';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -10,5 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
     <Enhancements />
     <AdvancedPlanner />
+    <CommunityResearchHub />
   </React.StrictMode>
 );
